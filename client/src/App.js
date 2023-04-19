32d2c69bc7de71dedf778736a47022ead43f1204
Christian Francini Mascagni <christian.francini87@gmail.com>
2023-04-19 23:17:14 +0200

    add component - pages - router

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import Header from './Header';
+import Layout from './Layout';
+import Login from './pages/Login';
 import Post from './Post';
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
 
 function App() {
     return (
-        <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-        </main>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route index element={<Home />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/register' element={<Register />} />
+            </Route>
+        </Routes>
     );
 }
 
---
+++ client/src/pages/Register/index.js
@@ -0,0 +1,14 @@
+const Register = () => {
+    return (
+        <form className='register'>
+            <h1>Register</h1>
+            <input type='text' placeholder='name' />
+            <input type='text' placeholder='surname' />
+            <input type='text' placeholder='username' />
+            <input type='password' placeholder='password' />
+            <button>Register </button>
+        </form>
+    );
+};
+
+export default Register;
